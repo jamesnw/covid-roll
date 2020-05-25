c9d46f32b8ee26dedbef7577fe0f703d26a11931
James Stuckey Weber <james.stuckeyweber@aunalytics.com>
2020-05-25 09:50:58 -0400

    Load by county name

--- composables/area.ts
+++ composables/area.ts
@@ -6,7 +6,7 @@ const ranges: Number[] = [1, 7, 14];
 const area = ref<String>("Elkhart");
 
 // @todo Get list of all counties
-const countyList = ref<String[]>(["St. Joseph", "Elkhart", "Marion"]);
+const countyList = ref<String[]>(["Adams", "Elkhart", "Marion"]);
 
 const dataframe = ref<Object[]>([]);
 // Initial load
@@ -16,10 +16,12 @@ async function loadCounty(county: String) {
   // Workaround cors
   const corsAnywhere = "https://cors-anywhere.herokuapp.com";
 
-  const base = "https://hub.mph.in.gov/api/3/action/datastore_search";
-  const resource = "resource_id=afaa225d-ac4e-4e80-9190-f6800c366b58";
+  const base = "https://hub.mph.in.gov/api/3/action/datastore_search_sql";
+  const resource = "afaa225d-ac4e-4e80-9190-f6800c366b58";
+  county = county.toUpperCase();
+  const sql = `sql=SELECT * FROM "${resource}" WHERE "COUNTY_NAME" LIKE '${county}'`;
   // const resource = "resource_id=afaa225d-ac4e-4e80-9190-f6800c366b58";
-  return await fetch(`${corsAnywhere}/${base}?${resource}&q=${county}`, {})
+  return await fetch(`${corsAnywhere}/${base}?${sql}`, {})
     .then((res) => res.json())
     .then((res) => {
       dataframe.value = res.result.records;
