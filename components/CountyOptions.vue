4084c4c8fdbda530081ab1e6c4729e06c9861549
James Stuckey Weber <james.stuckeyweber@aunalytics.com>
2020-07-08 22:07:22 -0400

    Add ids to selects

--- components/CountyOptions.vue
+++ components/CountyOptions.vue
@@ -1,5 +1,5 @@
 <template>
-  <select v-model="area" class="border rounded-sm mx-1">
+  <select v-model="area" id="county-selector" class="border rounded-sm mx-1">
     <option v-for="opt in countyList" :value="opt.key" :key="opt.key" :name="opt.name">{{opt.name}}</option>
   </select>
 </template>
