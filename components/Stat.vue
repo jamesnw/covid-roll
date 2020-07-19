3992fd781927c0f4589548ad5ad1e639a7522f11
James Stuckey Weber <james.stuckeyweber@aunalytics.com>
2020-07-19 15:59:35 -0400

    Fix close tag

--- components/Stat.vue
+++ components/Stat.vue
@@ -2,7 +2,7 @@
   <div class="w-1/4 mx-2 border-blue-900 border">
 	<h1 class="text-xl bg-blue-100">{{title}}</h1>
 	<div class="bg-blue-900 text-blue-100 text-4xl font-hairline">{{value}}</div>
-	<div class="bg-blue-100 text-xs" v-if="info">{{info}}</span>
+	<div class="bg-blue-100 text-xs" v-if="info">{{info}}</div>
   </div>
 </template>
 
